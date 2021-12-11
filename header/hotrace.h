1f557465d2bfe9e270d5299721f3ca2541416197
agautier <agautier@student.42.fr>
2021-12-11 17:10:41 +0100

    clean: norm main.c

--- header/hotrace.h
+++ header/hotrace.h
@@ -6,7 +6,7 @@
 /*   By: mamaquig <mamaquig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/11 11:52:46 by agautier          #+#    #+#             */
-/*   Updated: 2021/12/11 17:00:44 by agautier         ###   ########.fr       */
+/*   Updated: 2021/12/11 17:01:57 by agautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,6 @@ char	**ft_split(char const *s, char c);
 char	*ft_strjoin(const char *s1, const char *s2);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlen(const char *s);
-char	*gnl(void);
+char	*gnl(t_buffer *buf, int data);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,66 +6,48 @@
 /*   By: mamaquig <mamaquig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/11 10:03:38 by mderome           #+#    #+#             */
-/*   Updated: 2021/12/11 16:48:23 by agautier         ###   ########.fr       */
+/*   Updated: 2021/12/11 17:08:33 by agautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "hotrace.h"
+#include <stdlib.h>
 
-void    free_tab(char **tab)
+void	print_tab(char **tab)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (tab[i])
-    {
-        free(tab[i]);
-        i++;
-    }
-    free(tab);
-    tab = NULL;
+	i = 0;
+	while (tab[i])
+	{
+		write(1, &tab[i], ft_strlen(tab[i]));
+		i++;
+	}
 }
 
-void print_tab(char **tab)
-{
-    int i = 0;
-
-    while (tab[i])
-    {
-        write(1, &tab[i], ft_strlen(tab[i]));
-        i++;
-    }
-}
-
-int main(void)
+int	main(void)
 {
 	static t_buffer	buf;
 	int				data;
-    //char    **tab;
-	data = 1;
-	char *line = gnl(&buf, data);
+	char			*line;
 
+	data = 1;
+	line = gnl(&buf, data);
 	while (line)
 	{
 		printf("line = %s\n", line);
 		free(line);
-		line = gnl(&buf);
-		// sleep(1);
+		line = gnl(&buf, 1);
 	}
 	printf("fin de &buf\n\n");
 	printf("debut de recherche\n");
-	line = gnl(&buf);
+	line = gnl(&buf, 0);
 	while (line)
 	{
 		printf("line = %s\n", line);
 		free(line);
-		line = gnl(&buf);
-		// sleep(1);
+		line = gnl(&buf, 0);
 	}
 	free(line);
-    // tab = stock_file();
-    // tab = ft_split(tmp, '\n');
-    // print_tab(tab);
-    // free_tab(tab);
-    return (0);
+	return (EXIT_SUCCESS);
 }
