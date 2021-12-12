57fde2073135e6873d4f0ff955d49209306900ca
jraffin <jraffin@student.42.fr>
2021-12-12 17:22:49 +0100

    updated ressources

--- ressources/convert_ressources.sh
+++ ressources/convert_ressources.sh
@@ -6,7 +6,7 @@
 #    By: jraffin <jraffin@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/12/11 15:13:52 by jraffin           #+#    #+#              #
-#    Updated: 2021/12/11 15:46:45 by jraffin          ###   ########.fr        #
+#    Updated: 2021/12/12 17:19:10 by jraffin          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,5 +16,5 @@ for FILE in $1/*
 do
 	grep = $FILE | tr '=' '\n' > ./$(basename $FILE)
 	echo "" >> ./$(basename $FILE)
-	grep -v = $FILE >> ./$(basename $FILE)
+	grep -v = $FILE | grep -v ^! >> ./$(basename $FILE)
 done
